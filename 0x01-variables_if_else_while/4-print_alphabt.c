1a640d5f605952dd67a42f4b31b22a9353938b15
colezmann <koli.edim@gmail.com>
2022-10-15 07:26:10 -0700

    added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char x = 97;
+
+	while (x <= 122)
+	{
+		if (x != 113 && x != 101)
+			put char(x);
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
