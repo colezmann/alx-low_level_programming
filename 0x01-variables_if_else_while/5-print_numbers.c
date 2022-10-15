808e57435d170c53139a2e9ba2ffd2718adf18dd
colezmann <koli.edim@gmail.com>
2022-10-15 07:41:49 -0700

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	x = 0;
+	while (x <= 9)
+	{
+		putc(x);
+		x++;
+	}
+	putc(10);
+	return (0);
+}
