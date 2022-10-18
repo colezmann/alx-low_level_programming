ea64576fdc9103d83c08ce0b033af81bf1c3aaee
colezmann <koli.edim@gmail.com>
2022-10-18 10:45:07 -0700

    added 0-putchar.c and README.md

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char x = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(x[i]);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+DESCRIPTION OF TASKS FOR `0x02-functions_nested_loops` PROJECT
