3527cfacc8b2daa375add6854907c4ceb5fa7e54
colezmann <koli.edim@gmail.com>
2022-10-18 14:48:03 -0700

    modified 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,17 +4,9 @@
   * @n: integer to be computed.
   * Return: absolute value of n.
   */
-int _abs(int)
+int _abs(int n)
 {
-	int n;
-
 	if (n < 0)
-	{
 		n = -n;
-		return (n);
-	}
-	else
-	{
-		return (n);
-	}
+	return (n);
 }
