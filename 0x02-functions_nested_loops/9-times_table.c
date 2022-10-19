f59b17a3c4ea488d0e28cb6da049843e2ba74f13
colezmann <koli.edim@gmail.com>
2022-10-19 05:26:03 -0700

    added 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+  * times_table - Prints the 9-times table
+  * starting with 0
+  * Return: 10 x 10 (i x j) array
+  */
+void times_table(void)
+{
+	int i, j, prod;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+			if (prod == 0)
+			{
+				_putchar('0' + prod);
+			}
+			else if (prod < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + prod);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (prod / 10));
+				_putchar('0' + (prod % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
