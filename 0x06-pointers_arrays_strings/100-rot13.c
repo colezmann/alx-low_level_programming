cdbc5dee2279a760b56734e3b65d81f9e9e5eae3
colezmann <koli.edim@gmail.com>
2022-10-28 22:12:34 +0000

    added 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *rot13 - encodes a string using rot13
+ * @s: string to be encoded
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int x[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	int y[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (x[j] != s[i] && x[j] != '\0')
+			j++;
+		if (x[j] == s[i])
+			s[i] = y[j];
+	}
+	return (s);
+}
