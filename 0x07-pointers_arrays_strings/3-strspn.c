e405651eeaffe8133d18e0757365d4f561ffd0fe
colezmann <koli.edim@gmail.com>
2022-11-01 14:33:08 +0000

    added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be searched
+ * @accept: prefix to be measured
+ * Return: number of bytes, b in s which consist only
+ * of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, b = 0;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				b++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return b;
+		}
+	}
+	return b;
+}
