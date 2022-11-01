fdfe8e27dcba680305338c413360c30611fa679e
colezmann <koli.edim@gmail.com>
2022-11-01 14:37:52 +0000

    modified 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -19,7 +19,7 @@ unsigned int _strspn(char *s, char *accept)
 				b++;
 				break;
 			}
-			else if (accept[j + 1] == '\0')
+			else if (accept[j + 1] == '\0' && s[i] != accept[j])
 				return b;
 		}
 	}
