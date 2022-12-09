1d5ff83b411c0867630aacf52f8282e1ca030c43
colezmann <koli.edim@gmail.com>
2022-12-09 15:49:58 +0000

    revised 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+ * _strlen - determine length of string
+ * str - string whose length is to be determined
+ * Return: string length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len]; len++)
+		;
+	return (len);
+}
+
+/**
  * create_file - creates a file
  * @filename: name of the file to be created
  * @text_content: string to write to the file
@@ -7,21 +21,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, len;
+	int fd, wr;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len]; len++)
-			;
-	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(fd, text_content, len);
-
-	if (fd == -1 || wr == -1 || wr != len)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	wr = write(fd, text_content, _strlen(text_content));
+	if (wr == -1 || wr != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
 
 	close(fd);
 	return (1);
