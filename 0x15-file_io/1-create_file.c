d18b6de0ef5814714015d04006334e8f456303a6
colezmann <koli.edim@gmail.com>
2022-12-09 15:25:26 +0000

    modified 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
 
-	if (fd == -1 || wr == -1)
+	if (fd == -1 || wr == -1 || wr != len)
 		return (-1);
 
 	close(fd);
