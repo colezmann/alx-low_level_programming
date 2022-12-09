a5757fd2e6197d15ac5e7b2dfbf2f0db3c97bea6
colezmann <koli.edim@gmail.com>
2022-12-09 14:55:09 +0000

    satisfied betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
-	
+
 	if (fd == -1 || wr == -1)
 		return (-1);
 
