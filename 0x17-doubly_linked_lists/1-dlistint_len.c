ef74642c88d4f498de03318e1cc6c5d135af2798
colezmann <koli.edim@gmail.com>
2023-01-15 11:58:19 +0000

    betty compliant

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,13 +6,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-		size_t num_nodes = 0;
+	size_t num_nodes = 0;
 
-		while (h)
-		{
-				h = h->next;
-				num_nodes++;
-		}
+	while (h)
+	{
+		h = h->next;
+		num_nodes++;
+	}
 
-		return (num_nodes);
+	return (num_nodes);
 }
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,19 +7,19 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-		dlistint_t *new;
+	dlistint_t *new;
 
-		new = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 
-		if (new == NULL)
-				return (NULL);
+	if (new == NULL)
+		return (NULL);
 
-		new->n = n;
-		new->prev = NULL;
-		new->next = *head;
-		if (*head != NULL)
-				(*head)->prev = new;
-		*head = new;
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
 
-		return (new);
+	return (new);
 }
